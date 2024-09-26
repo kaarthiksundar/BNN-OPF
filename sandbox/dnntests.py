0507032c46475ac7a39b427d52fdf829d1b95223
Abhijith <abhijithj@lanl.gov>
2024-09-26 17:22:29 -0600

    Added working torch dnn example in sandbox

--- sandbox/dnntests.py
+++ sandbox/dnntests.py
@@ -1,11 +1,18 @@
+import sys
+sys.path.append('src')
 import typer
 from typing_extensions import Annotated
+from logger import CustomFormatter
 from pathlib import Path
 import logging
 import json
 import math
-
-from logger import CustomFormatter
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
 from dataloader import load_data
 from acopf import *
 from bnncommon import *
@@ -16,12 +23,39 @@ from classes import SampleCounts
 from jax import random
 from modelio import *
 
+
 def roundup(x):
     return int(math.ceil(x / 100.0)) * 100
 
-app = typer.Typer(pretty_exceptions_show_locals=False)
+class MLP(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_hidden_layers, output_dim, dim_reduction_layer = False):
+        super(MLP, self).__init__()
+        self.activation = nn.ReLU()
+        self.nhidden = num_hidden_layers
+        self.dim_red = dim_reduction_layer
+        self.fclayers = nn.ModuleList([nn.Linear(input_dim, hidden_dim)])
+        for d in range(num_hidden_layers-1):
+                self.fclayers.append(nn.Linear(hidden_dim, hidden_dim))
+        if dim_reduction_layer == True:
+            dim_red = roundup(0.7*output_dim)
+            self.fclayers.append(nn.Linear(hidden_dim, dim_red))
+            self.fclayers.append(nn.Linear(dim_red, output_dim))
+        else:
+            self.fclayers.append(nn.Linear(hidden_dim, output_dim))
 
-@app.command()
+    def forward(self, x):
+        for d in range(self.nhidden):
+            x = self.fclayers[d](x)
+            x = self.activation(x)
+        if self.dim_red:
+            x = self.fclayers[self.nhidden](x)
+            x = self.activation(x)
+            x = self.fclayers[self.nhidden+1](x)
+        else:
+            x = self.fclayers[self.nhidden](x)
+        return x
+
+
 def main(
     data_path: Annotated[str, typer.Option('--datapath', '-p')] = './data/', 
     case: Annotated[str, typer.Option('--case', '-c')] = 'pglib_opf_case118_ieee',
@@ -46,31 +80,32 @@ def main(
     error: Annotated[bool, typer.Option(help = 'error flag')] = False, 
     only_dl_flag: Annotated[bool, typer.Option(
         '--onlydl', help = 'only download data and exit')] = False) -> None:
-    
+       
     if (debug and warn) or (warn and error) or (debug and warn): 
-        print(f'only one of --debug, --warn, --error flags can be set')
-        return 
+       print(f'only one of --debug, --warn, --error flags can be set')
+       return 
     
     log = get_logger(debug, warn, error)
     
-    # cli-arg validation
+    #cli-arg validation
     loaded_cases = ['pglib_opf_case30_ieee', 'pglib_opf_case57_ieee',
-                    'pglib_opf_case118_ieee', 'pglib_opf_case500_goc']
+                   'pglib_opf_case118_ieee', 'pglib_opf_case500_goc']
     if case not in loaded_cases:
-        log.error(f'{case} can be only lie in {loaded_cases}')
-        return 
+       log.error(f'{case} can be only lie in {loaded_cases}')
+       return 
     
     possible_run_types = ['semisupervisedBNN', 'supervisedBNN', 'supervisedDNN']
     if run_type not in possible_run_types: 
-        log.error(f'{run_type} can only lie in {possible_run_types}')
-        return
+       log.error(f'{run_type} can only lie in {possible_run_types}')
+       return
     
     if (Path(data_path + config_file).is_file() == False): 
-        log.error(f'File {data_path + config_file} does not exist')
-        return
+       log.error(f'File {data_path + config_file} does not exist')
+       return
     
     data = json.load(open(data_path + config_file))
     batch_size = data["batch_size"]
+
     
     # follows a 80 % train, 20 % validation and test data is separate
     log.info(f'num groups to dl: {num_groups}')
@@ -127,55 +162,87 @@ def main(
         return
     
     rng_key = random.PRNGKey(0)
-    vi_parameters = None
+    X_train = np.asarray(opf_data.X_train)
+    Y_train = np.asarray(opf_data.Y_train)
+    X_val = torch.tensor(np.asarray(opf_data.X_val))
+    Y_val = torch.tensor(np.asarray(opf_data.Y_val))
+    X_test = torch.tensor(np.asarray(opf_data.X_test))
+    Y_test = torch.tensor(np.asarray(opf_data.Y_test))
+    dataset = TfData(X_train, Y_train)
+    trainloader=DataLoader(dataset=dataset,batch_size=opf_data.batch_size)
+    input_dim = X_train.shape[1]
+    output_dim = Y_train.shape[1]
+    hidden_dim = roundup(1.5*output_dim)
+    num_hidden = 2
+    learning_rate = 0.001
+    num_epochs = 600
+    model = MLP(input_dim, hidden_dim, num_hidden,  output_dim)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    losses = []
+    val_losses = []
+    val_feasibility = []
+    scheduler = optim.lr_scheduler.PolynomialLR(optimizer, total_iters=num_epochs, power=1)
+    lambda_l1 = 1E-4
+    equality_penalty = 0.0
+    inequality_penalty = 0.0
+    pytorch_total_params = sum(p.numel() for p in model.parameters())
+    for epoch in range(num_epochs):
+        for X_batch, Y_batch in trainloader:
+            l1_regularization = 0.
+            for param in model.parameters():
+                l1_regularization += param.abs().sum()
+            outputs = model(X_batch)
+            loss = criterion(outputs, Y_batch) + (lambda_l1/pytorch_total_params)*l1_regularization
+            # Backward pass and optimization
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        scheduler.step()
+
+        losses.append(loss.item())
+        Y_pred_val = model(X_val).detach()
+        val_loss = criterion(Y_pred_val, Y_val)
+        val_mean_feas = np.mean(assess_feasibility(np.asarray(X_val),Y_pred_val.numpy(), opf_data))
+        val_losses.append(val_loss.item())
+        val_feasibility.append(val_mean_feas)
+
+        if (epoch+1) % 50 == 0:
+            print(f'Epoch [{epoch+1}/{num_epochs}], val_loss: {val_loss.item():.4f}')
+
+# Plot the loss curve
     
-    if run_type == 'semisupervisedBNN':
-        vi_parameters = run_sandwich(
-            opf_data, log, 
-            config = data,
-            rng_key = rng_key
-        )
-    elif run_type == 'supervisedBNN': 
-        # supervised only 
-        initial_learning_rate = data.get("initial_learning_rate", 1e-3)
-        decay_rate = data.get("decay_rate", 1e-4) 
-        max_training_time = data.get("max_training_time", 1000.0)
-        max_epochs = data.get("max_epochs", 200) 
-        early_stopping_trigger_supervised = data.get("early_stopping_trigger_supervised", 25) 
-        patience_supervised = data.get("patience_supervised", 3)
-        supervised_early_stopper = PatienceThresholdStoppingCriteria(
-            log, patience = patience_supervised)
-        vi_parameters = None 
-        run_supervised(
-            opf_data, log, 
-            initial_learning_rate = initial_learning_rate, 
-            decay_rate = decay_rate, 
-            max_training_time = max_training_time, 
-            max_epochs = max_epochs, 
-            validate_every = early_stopping_trigger_supervised, 
-            vi_parameters = vi_parameters, 
-            stop_check = supervised_early_stopper, 
-            rng_key = rng_key
-        )
-        vi_parameters = unsupervised_early_stopper.vi_parameters 
-    elif run_type == 'supervisedDNN':
-        initial_learning_rate = data.get("initial_learning_rate", 1e-3)
-        decay_rate = data.get("decay_rate", 1e-4) 
-        max_training_time = data.get("max_training_time", 1000.0)
-        max_epochs = data.get("max_epochs", 200) 
-        early_stopping_trigger_supervised = data.get("early_stopping_trigger_supervised", 25) 
-        patience_supervised = data.get("patience_supervised", 3)
-        supervised_early_stopper = PatienceThresholdStoppingCriteria(
-            log, patience = patience_supervised)
-        vi_parameters = None 
-        ##Add code here
+    Y_pred = model(X_test)
+    test_loss = criterion(Y_pred, Y_test)
+    feasibility_violation = assess_feasibility(np.asarray(X_test), Y_pred.detach().numpy(), opf_data)
+
+    print(f'Test MSE: {test_loss.item():.4f}')
+    print(f'Test max feasibility violation:{np.max(feasibility_violation):.4f}')
+    print(f'Test mean feasibility violation:{np.mean(feasibility_violation):.4f}')
+    plt.plot(losses,label = "train MSE")
+    plt.plot(val_losses, label = "validattion MSE")
+    plt.plot(val_feasibility, label = "val mean feasibility")
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.yscale('log')
+    plt.legend()
+    plt.title('Training Loss/Last batch')
+    plt.show()
 
 
-    config = config_file.split('.')[0]
-    output_file = f'./output/{case}_{num_groups}_{num_train_per_group}_{run_type}_{config}.pkl'
-    write_to_file(output_file, rng_key, sample_counts, vi_parameters)  
-    # run_test(opf_data, rng_key, vi_parameters, log)
-    
+class TfData(Dataset):
+    def __init__(self,X_train, Y_train):
+        self.num_samples = X_train.shape[0]
+        self.X = torch.tensor(X_train, dtype=torch.float32)
+        self.Y = torch.tensor(Y_train, dtype=torch.float32)
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, idx):
+        return self.X[idx,:], self.Y[idx,:]
+
+
+
 
 def get_logger(debug, warn, error): 
     log = logging.getLogger('bnn-opf')
@@ -199,6 +266,7 @@ def get_logger(debug, warn, error):
     log.addHandler(fh) 
     return log
 
+output = main()        
 
-if __name__ == "__main__":
-    app()
+ # Define the MLP model
+
